7e174875618d82aecb41174f41ffa56e63ea6e1f
Kara Rainey <karain82@hotmail.com>
2018-05-13 00:19:01 -0500

    creating homepage skeleton for thoughtworks.com no CSS yet

--- src/Components/Day1/Day1.js
+++ src/Components/Day1/Day1.js
@@ -21,10 +21,46 @@ export default class Day1 extends Component{
                         <a>Deutsch</a>
                         <a>中文</a>
                     </nav>
-                    <input type="text" placeholder="Search"/>
-                    <a>News</a>
+                    <input type="text" placeholder="Search thoughtworks.com"/>
+                    <button type="submit"></button>
                     <a>Contact Us</a>
                 </header>
+                <main>
+                    <div>
+                        Thoughtworks
+                        <nav>
+                            <a>Clients</a>
+                            <a>Services</a>
+                            <a>Products</a>
+                            <a>Insights</a>
+                            <a>Events</a>
+                            <a>About Us</a>
+                            <a>Careers</a>
+                        </nav>
+                    </div>
+                    <article>
+                        <section>ALWAYS INVENTING, ALWAYS DELIVERING</section>
+                        <section>Great ideas, brought to life with technology. In weeks, not years.</section>
+
+                        <a>Technology Radar Volume 18
+                            <p>Subscribe Now</p>
+                            <img src=""/>
+                        </a>
+                        <a>Intelligent Empowerment
+                            
+                            <img src=""/>
+                        </a>
+                        <a>
+                            <img src=""/>
+                        </a>
+                        <a>
+                            <img src=""/>
+                        </a>
+                        <a>
+                            <img src=""/>
+                        </a>
+                    </article>
+                </main>
             </div>
         )
     }
