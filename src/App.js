c78c571241bcb8ddb80a704f7bf2f2029d9e8e72
Kara Rainey <karain82@hotmail.com>
2018-05-29 17:42:32 -0500

    building a chat app using Redux and sockets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 import Day1 from "./Components/Day1/Day1";
+import ChatBox from "./Components/ChatBox/ChatBox";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">      
-        <Day1/>
+        <ChatBox/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+import chat from "./reducers";
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(chat)
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
