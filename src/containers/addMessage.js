fb64626696bb997192c83e8f469df534a4878200
Kara Rainey <karain82@hotmail.com>
2018-05-29 18:38:50 -0500

    building reducers and containers for messaging

---
+++ src/containers/addMessage.js
@@ -0,0 +1,28 @@
+import react from 'react';
+import propTypes from 'prop-types';
+
+const AddMessage = (props) => {
+    let input
+
+    return(
+        <section id="new-message"
+        <input
+            onKeyPress={(e) => {
+                if (e.key === 'Enter'){
+                    props.dispatch(input.value, 'Me')
+                    input.value = ""
+                }
+            }}
+            type="text"
+            ref={(node) => {
+                input = node
+            }}>
+            </section>
+    )
+}
+
+AddMessage.PropTypes = {
+    dispatch: PropTypes.func.isRequired
+}
+
+export default AddMessage;
